94c7be82ec00ba86bde748c8a413649d24574a2c
Kellvem Barbosa <kellvem222@gmail.com>
2023-04-24 01:05:42 +0100

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,59 +1,78 @@
-import { createBlock, createConfig, createGroup, createPanel } from './config.builder';
+import { Dashboard, Label, createBlock, createConfig, createGroup, createLabel, createPanel } from './config.builder';
 import { EditBoolean, EditFile, EditInterface, EditPlaintext, EditSelect, ListPlaintext, ViewFile } from './config.interfaces';
 
 export default createConfig('https://cloud.knksolutions.pt/v1', '63f246ea28374c583dd5')
-
-	.addGroup(
-	createGroup()
-		.addPanel(
-		createPanel('6442cfe96229be0ea6ba', 'watchfaces')
-			//.addIdBlock()
-			.addBlock(createBlock('name'))
-			.addBlock(
-				createBlock('isPremium')
-				.setEditInterface(new EditBoolean().setDefaultChecked(true))
-			)
-			.addBlock(
-				createBlock('showClock')
-				.setEditInterface(new EditBoolean().setDefaultChecked(false))
-			)
-			.addBlock(
-				createBlock('watchMedia')
-				.setEditInterface(new EditFile("app"))
-				.setViewInterface(new ViewFile("app"))
-			)
-			.addBlock(
-				createBlock('watchFile')
-				.setEditInterface(new EditFile("app"))
-				.setViewInterface(new ViewFile("app"))
-			)
-			.addBlock(
-				createBlock('fileType')
-				.setEditInterface(new EditPlaintext().setCreateValue("image"))
-			)
-			.addBlock(
-				createBlock('faceType')
-				.setEditInterface(
-					new EditSelect()
-						.setOptions([
-							{ value: 'file', label: 'File' },
-							{ value: 'builder', label: 'Builder' }
-						])
-						.setDefaultOption('file')
-				)
-			)
-			.addBlock(
-				createBlock('faceModel')
-				.setEditInterface(
-					new EditSelect()
-						.setOptions([
-							{ value: 'photo', label: 'Photo Basic' },
-							{ value: 'live', label: 'Live Basic' },
-							{ value: 'portrait', label: 'Portrait' },
-							{ value: 'meridian', label: 'Meridian' }
-						])
-						.setDefaultOption('portrait')
-				)
-			)
+	.setName('AppwriteCMS')
+	.setIcon('🚀')
+	.setDashboard(
+		new Dashboard()
+		.setMotd('Welcome to the AppwriteCMS demo!')
 	)
-);
+	.addGroup(
+		createGroup()
+			.setName('Newipe')
+			.setIcon('👋')
+			.addPanel(
+				createPanel('6442cfe96229be0ea6ba', 'watchfaces')
+					//.addIdBlock()
+					.addDefaultLabel()
+					.addLabel(createLabel("Premium").setIcon('🔒').setQueries(["isPremium"]))
+					.setName('Watchfaces')
+					.setIcon('⌚️')
+					.addBlock(createBlock('name'))
+					.addBlock(
+						createBlock('isPremium')
+							.setEditInterface(new EditBoolean().setDefaultChecked(true))
+					)
+					.addBlock(
+						createBlock('showClock')
+							.setEditInterface(new EditBoolean().setDefaultChecked(false))
+					)
+					.addBlock(
+						createBlock('watchMedia')
+							.setEditInterface(new EditFile("app"))
+							.setViewInterface(new ViewFile("app"))
+					)
+					.addBlock(
+						createBlock('watchFile')
+							.setEditInterface(new EditFile("app"))
+							.setViewInterface(new ViewFile("app"))
+					)
+					.addBlock(
+						createBlock('fileType')
+							.setEditInterface(
+								new EditSelect()
+									.setOptions([
+										{ value: 'image', label: 'Image' },
+										{ value: 'mp4', label: 'MP4' },
+										{ value: 'gif', label: 'GIF' }
+									])
+									.setDefaultOption('image')
+							)
+					)
+					.addBlock(
+						createBlock('faceType')
+							.setEditInterface(
+								new EditSelect()
+									.setOptions([
+										{ value: 'file', label: 'File' },
+										{ value: 'builder', label: 'Builder' }
+									])
+									.setDefaultOption('file')
+							)
+					)
+					.addBlock(
+						createBlock('faceModel')
+							.setEditInterface(
+								new EditSelect()
+									.setOptions([
+										{ value: 'photo', label: 'Photo Basic' },
+										{ value: 'live', label: 'Live Basic' },
+										{ value: 'portrait', label: 'Portrait' },
+										{ value: 'meridian', label: 'Meridian' }
+									])
+									.setDefaultOption('portrait')
+							)
+					)
+			)
+	);
